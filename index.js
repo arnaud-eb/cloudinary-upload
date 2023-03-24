26b7d9633d9407b80dc7f5bf2c5fc029d6d07772
Teri Eyenike <teyenike1@gmail.com>
2023-03-24 14:19:15 +0100

    Update: modify the sample code

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const cloudinary = require('./config');
+const path = require("path");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("./config");
 
 // dotenv
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 // middleware
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(
   fileUpload({
@@ -23,38 +23,38 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Uploading images to Cloudinary Console',
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Uploading images to Cloudinary Console",
   });
 });
 
-app.post('/upload', async (req, res) => {
+app.post("/upload", async (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     res.status(400).json({
-      msg: 'No files were uploaded. Try uploading an image',
+      msg: "No files were uploaded. Try uploading an image",
     });
     return;
   }
 
   const uploadFile = req.files.uploadFile;
+  console.log(uploadFile);
 
   const result = await cloudinary.uploader.upload(uploadFile.tempFilePath, {
     public_id: uploadFile.name,
-    resource_type: 'auto',
-    folder: 'uploaded',
+    resource_type: "auto",
+    folder: "uploaded",
     use_filename: true,
     unique_filename: false,
   });
 
   if (result.url) {
-    res.render('media', {
+    res.render("media", {
       img: result.url,
-      name: uploadFile.name.replace(/.jpeg|.jpg|.png|.webp/gi, ''),
+      name: uploadFile.name.replace(/.jpeg|.jpg|.png|.webp/gi, ""),
     });
-  }
-  else {
-    res.render('/upload');
+  } else {
+    res.render("/upload");
   }
 });
 
